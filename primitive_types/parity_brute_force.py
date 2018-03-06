737523c6130f90f1a9b3c2fa304c997aaaeb6f51
Noah Lidell <noahlidell@gmail.com>
2018-03-05 22:16:45 -0600

    brute force solution for finding the parity of the binary representation of an integer

---
+++ primitive_types/parity_brute_force.py
@@ -0,0 +1,25 @@
+def parity(x):
+    """
+    Returns the parity of a positive integer: returns
+    1 if there are an odd number of 1s in the binary representation
+    of the integer, returns 0 if the number of 1s is even
+    """
+    parity = 0
+
+    while x:
+        parity ^= x & 1
+        x >>= 1
+    return parity
+
+def main():
+    import sys
+    if len(sys.argv) == 2:
+        num = int(sys.argv[1])
+    else: 
+        num = 12
+    print("{} in binary is {}".format(num,bin(num)))
+    print("parity is {}".format(parity(num)))
+
+if __name__ == "__main__":
+    main()
+

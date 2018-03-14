2f9714ff3a8b70afb46c7be41eb5bd550dac3eb9
nlidell <noahlidell@gmail.com>
2018-03-14 00:34:12 -0400

    gitignore, plus first half of work on script to reverse 64 bit word

---
+++ primitive_types/reverse_bits.py
@@ -0,0 +1,19 @@
+
+def switch_bits(x,i,j):
+    if x >> i & 1 != x >> j & 1:
+        x ^= 1 << i
+        x ^= 1 << j
+    return x
+
+def init_16bit_lookup():
+    lookup = {}
+    for x in range(0xFFFF+1):
+        original = x
+        for i in range(8):
+            x = switch_bits(x,i,15-i)
+        lookup[original] = x
+    return lookup
+
+def reverse_64bit_int(x):
+
+
